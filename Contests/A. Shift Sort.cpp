1c7d6754e674e106eee663af906c1b4f28ea5342
Nazrul Islam <mhdnazrul511@gmail.com>
2025-09-09 22:37:30 +0600

    Add solution

---
+++ Contests/A. Shift Sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+const ll mod = 1e9 + 7;
+void solve()
+{
+ int n;
+ cin >> n;
+ string s;
+ cin >> s;
+
+ int zero = 0;
+ for (char c : s)
+ {
+  if (c == '0')
+  {
+   zero++;
+  }
+ }
+
+ int one = 0;
+ for (int i = 0; i < zero; ++i)
+ {
+  if (s[i] == '1')
+  {
+   one++;
+  }
+ }
+
+ cout << one << endl;
+}
+
+int main() {
+  FAST_IO;
+  int T; cin >> T;while(T--)
+    solve();
+  return 0;
+}
--- difficulty-800-1000/A. Bit++.cpp
+++ difficulty-800-1000/A. Bit++.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     int n;cin>>n;
-    //vector<string> str(n);
     int cnt = 0;
     while(n--){
         string str;cin>>str;
@@ -14,4 +13,5 @@ int main()
         }
     }
     cout<<cnt<<endl;
+    return 0;
 }
