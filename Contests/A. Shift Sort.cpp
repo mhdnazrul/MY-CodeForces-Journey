f1f8c5c175ea1b9e136bab416f7413142e6f4428
Nazrul Islam <mhdnazrul511@gmail.com>
2025-09-13 04:21:59 +0600

    Add solution

--- Contests/A. Shift Sort.cpp
+++ Contests/A. Shift Sort.cpp
@@ -1,41 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define FAST_IO                \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
 using ll = long long;
 using vi = vector<int>;
 using vll = vector<ll>;
 const ll mod = 1e9 + 7;
 void solve()
 {
- int n;
- cin >> n;
- string s;
- cin >> s;
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
 
- int zero = 0;
- for (char c : s)
- {
-  if (c == '0')
+  int zero = 0;
+  for (char c : s)
   {
-   zero++;
+    if (c == '0')
+    {
+      zero++;
+    }
   }
- }
 
- int one = 0;
- for (int i = 0; i < zero; ++i)
- {
-  if (s[i] == '1')
+  int one = 0;
+  for (int i = 0; i < zero; ++i)
   {
-   one++;
+    if (s[i] == '1')
+    {
+      one++;
+    }
   }
- }
 
- cout << one << endl;
+  cout << one << endl;
 }
 
-int main() {
+int main()
+{
   FAST_IO;
-  int T; cin >> T;while(T--)
+  int T;
+  cin >> T;
+  while (T--)
     solve();
   return 0;
 }
