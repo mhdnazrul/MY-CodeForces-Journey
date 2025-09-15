77efa17538567581651b8716d40be814e8484022
Nazrul Islam <mhdnazrul511@gmail.com>
2025-09-15 17:04:21 +0600

    Add Solution

---
+++ difficulty-800-1000/A. Tram.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/problemset/problem/116/A
+// Nazrul Islam
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO               \
+ ios::sync_with_stdio(false); \
+ cin.tie(nullptr);
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+const ll mod = 1e9 + 7;
+
+void solve()
+{
+ int T, cnt = 0, ans = 0;
+ cin >> T;
+ while (T--)
+ {
+  int out, in;
+  cin >> out >> in;
+  cnt -= out;
+  cnt += in;
+  ans = max(ans, cnt);
+ }
+ cout << ans << endl;
+}
+
+int main()
+{
+ FAST_IO;
+ solve();
+ return 0;
+}
---
+++ difficulty-800-1000/A. Vanya and Fence.cpp
@@ -0,0 +1,37 @@
+// https://codeforces.com/problemset/problem/677/A
+// Nazrul Islam
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO               \
+ ios::sync_with_stdio(false); \
+ cin.tie(nullptr);
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+const ll mod = 1e9 + 7;
+
+void solve()
+{
+ int n, h;
+ cin >> n >> h;
+ int ans = 0;
+ for (int i = 0; i < n; ++i)
+ {
+  int t;
+  cin >> t;
+  if (t <= h)
+   ans++;
+  else
+   ans += 2;
+ }
+ cout << ans << endl;
+}
+
+int main()
+{
+ FAST_IO;
+ // int T; cin >> T;while(T--)
+ solve();
+ return 0;
+}
