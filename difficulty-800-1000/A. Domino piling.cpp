1582de603861627b9e181a747ed27cae3da53df0
mhdNazruL <nazru.puc.cse@gmail.com>
2025-10-10 05:35:06 +0600

    Add Solution

--- difficulty-800-1000/A. Domino piling.cpp
+++ difficulty-800-1000/A. Domino piling.cpp
@@ -1,15 +1,10 @@
 // https://codeforces.com/problemset/problem/158/A
-// Nazrul Islam
+// nazrulislam_7
 #include <bits/stdc++.h>
 using namespace std;
 #define FAST_IO               \
  ios::sync_with_stdio(false); \
  cin.tie(nullptr);
-using ll = long long;
-using vi = vector<int>;
-using vll = vector<ll>;
-const ll mod = 1e9 + 7;
-
 void solve()
 {
  int m, n;
